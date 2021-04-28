c8586942825624c5416f9dfad7e35b6f0ebd76e6
Hurshid Hujamatov <hujamatov_h@free.uz>
2021-04-28 15:09:22 +0500

    Added calculate function and extended constructor for sphera.h

--- widgets/sphera.cpp
+++ widgets/sphera.cpp
@@ -71,3 +71,72 @@ Sphera::Sphera(double radius, QVector3D position)
     m_texture->setMagnificationFilter(QOpenGLTexture::Linear);
     m_texture->setWrapMode(QOpenGLTexture::Repeat);
 }
+
+Sphera::Sphera(double radius, int arc, int highLat, int lowLat, QQuaternion rotation, QVector3D position)
+{
+    m_radius = fabs(radius);
+    m_rotation = rotation;
+    m_position = position;
+    m_arc = arc % 360 == 0 ? 360 : arc % 360;
+    m_highLatitude = max(min(90, highLat), -90);
+    m_lowLatitude  = max(min(90, lowLat ), -90);
+    if (m_highLatitude < m_lowLatitude) {
+        m_highLatitude += m_lowLatitude;
+        m_lowLatitude = m_highLatitude - m_lowLatitude;
+        m_highLatitude = m_highLatitude - m_lowLatitude;
+    } else if (m_lowLatitude == m_highLatitude) {
+        m_highLatitude = min(m_highLatitude + 1, 90);
+        m_lowLatitude  = max(m_lowLatitude - 1, -90);
+    }
+
+    calculate();
+}
+
+void Sphera::calculate()
+{
+    QVector<VertexData> vertexes;
+    QVector<GLuint> indexes;
+
+    int lonStep = 1;
+    int latStep = 1;
+
+    for (int lat = -90; lat < -20; lat += latStep) {
+        for (int lon = 0; lon < 360; lon += lonStep) {
+            double rr = radius;
+            auto verCoord1 = calcPoint(lon, lat);
+            auto texCoord1 = QVector2D((verCoord1.x() + 1) / 2.0f, (verCoord1.y() + 1) / 2.0f);
+            auto vertData1 = VertexData(verCoord1 * rr + position, texCoord1);
+            vertexes.append(vertData1);
+
+            auto verCoord2 = calcPoint(lon + lonStep, lat);
+            auto texCoord2 = QVector2D((verCoord2.x() + 1) / 2.0f, (verCoord2.y() + 1) / 2.0f);
+            auto vertData2 = VertexData(verCoord2 * rr + position, texCoord2);
+            vertexes.append(vertData2);
+
+
+            auto verCoord3 = calcPoint(lon + lonStep, lat + latStep);
+            auto texCoord3 = QVector2D((verCoord3.x() + 1) / 2.0f, (verCoord3.y() + 1) / 2.0f);
+            auto vertData3 = VertexData(verCoord3 * rr + position, texCoord3);
+            vertexes.append(vertData3);
+
+            auto verCoord4 = calcPoint(lon, lat + latStep);
+            auto texCoord4 = QVector2D((verCoord4.x() + 1) / 2.0f, (verCoord4.y() + 1) / 2.0f);
+            auto vertData4 = VertexData(verCoord4 * rr + position, texCoord4);
+            vertexes.append(vertData4);
+        }
+    }
+
+    for (int i = 0; i < vertexes.size(); i ++) {
+        indexes.push_back(static_cast<uint>(i));
+    }
+
+    m_arrayBuffer.create();
+    m_arrayBuffer.bind();
+    m_arrayBuffer.allocate(vertexes.constData(), static_cast<int>(vertexes.size()) * static_cast<int>(sizeof (VertexData)));
+    m_arrayBuffer.release();
+
+    m_indexBuffer.create();
+    m_indexBuffer.bind();
+    m_indexBuffer.allocate(indexes.constData(), static_cast<int>(indexes.size()) * static_cast<int>(sizeof (GLuint)));
+    m_indexBuffer.release();
+}
--- widgets/sphera.h
+++ widgets/sphera.h
@@ -7,8 +7,15 @@ class Sphera : public DrawableObject
 {
 public:
     Sphera(double radius = 1, QVector3D position = QVector3D(0, 0, 0));
+    Sphera(double radius = 1, int arc = 360, int highLat = 90, int lowLat = -90,
+           QQuaternion rotation = QQuaternion(), QVector3D position = QVector3D(0, 0, 0));
 private:
+    int m_arc;
+    double m_radius;
+    int m_highLatitude;
+    int m_lowLatitude;
 
+    void calculate();
 };
 
 #endif // SPHERA_H
